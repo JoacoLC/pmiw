4ae076b006f7933dd5226d0bdb7cffd2eb073e4c
Joaquín Labanda <joaquinlabanda04@gmail.com>
2026-09-01 16:47:53 -0300

    Se empieza a dibujar el fondo

---
+++ fondo.js
@@ -0,0 +1,31 @@
+function dibuj_piso(x, y, ancho, alto, escala) {
+  for (i = 0; i <= alto; i++) for (j = 0; j <= ancho; j++) {
+    image (
+      floor,
+      x + j * (floor.width * abs(escala)),
+      y + i * (floor.height * abs(escala)),
+      floor.width * escala,
+      floor.height * escala
+      );
+  }
+}
+
+function dibuj_pilar(x, y, altura, escala) {
+  for (i = 0; i < altura; i++) {
+    image (
+      pilar_top,
+      x,
+      y + i * (floor.height * abs(escala)),
+      pilar_top.width * escala,
+      pilar_top.height * escala
+    );
+  }
+  
+  image (
+    pilar_bottom,
+    x,
+    y + altura * pilar_top.height * abs(escala),
+    pilar_bottom.width * escala,
+    pilar_bottom.height * escala
+  );
+}
--- tp1.js
+++ tp1.js
@@ -1,8 +1,29 @@
-function setup() {
+let ness_walk;
+let ness_sit;
+let ness_lie;
+let cop_walk;
 
+let floor;
+let halfplant;
+let halfwin;
+let pilar_top;
+let pilar_bottom;
+
+function preload() {
+  floor        = loadImage("data/floorpattern.png");
+  pilar_top    = loadImage("data/pillar_top.png");
+  pilar_bottom = loadImage("data/pillar_bottom.png");
+}
+
+function setup() {
+  createCanvas(800,600);
+  noSmooth();
+  
+  background(75, 75, 150);
 }
 
 
 function draw() {
-
+  dibuj_piso(0,384, 16,8, 3);
+  dibuj_pilar(0,0, 13, 3,3);
 }
